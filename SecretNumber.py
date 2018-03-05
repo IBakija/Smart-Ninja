5d4cecf13dff872cfb14df4f626fdf3c8bed3529
IGBakija <ivana.g.bakija@gmail.com>
2018-03-05 09:54:54 +0100

    first commit

---
+++ SecretNumber.py
@@ -0,0 +1,24 @@
+import random
+
+while True:
+    random_num = random.randint(1, 10)
+    broj_pokusaja = 3
+    while True:
+        for i in range(broj_pokusaja):
+            print "Pokusaj " + str(i + 1) + "/" + str(broj_pokusaja)
+            pokusaj = int(raw_input("Guess the secret number: "))
+            if pokusaj == random_num:
+                print "Bravo!"
+                break
+            elif pokusaj > random_num:
+                print "Try a smaller number."
+            else:
+                print "Try a bigger number."
+            jos = ""
+        if pokusaj != random_num:
+            jos = raw_input("zelite li jos jednom pokusati? (y/n): ")
+        if jos != "y" or pokusaj == random_num:
+            break
+    josigra = raw_input("Jos jedna igra? (y/n): ")
+    if josigra != "y":
+        break
